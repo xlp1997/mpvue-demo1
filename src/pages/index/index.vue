45b12c19bb094db82db4722a1bfbcba16b6aeb87
xiao.liping@foxmail.com <xiao.liping@foxmail.com>
2018-08-12 21:35:42 +0800

    第一次提交

---
+++ src/pages/index/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="message"  @click="clickHandle">{{msg}}</div>
+    <swiper indicator-dots="false" 
+  autoplay="false" interval="5000" duration="1000" style="text-align:center">
+  <block v-for="(item,index) in imgUrls" :key="index">
+    <swiper-item>
+      <image  @click="onItemClick(index)" :src="item" class="slide-image" width="355" height="150"/>
+    </swiper-item>
+  </block>
+</swiper>
+
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello',
+      imgUrls: [
+        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ]
+      // indicatorDots: false,
+      // autoplay: false,
+      // interval: 5000,
+      // duration: 1000
+    }
+  },
+
+  methods: {
+    clickHandle () {
+      this.msg = 'Clicked!!!!!!'
+    },
+    onItemClick: function (event) {
+      console.log('index' + '-----' + event)
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+.message {
+  color: red;
+  padding: 10px;
+  text-align: center;
+}
+</style>
